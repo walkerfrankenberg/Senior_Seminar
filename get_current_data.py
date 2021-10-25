1c5d8f99bce6c6a5c21c4e1f51fb3a79316b4f3b
walkerfrankenberg <wfrankenberg@middlebury.edu>
2021-10-25 13:18:32 -0400

    work

--- get_current_data.py
+++ get_current_data.py
@@ -1,5 +1,24 @@
 import requests
+from datetime import datetime
+import pandas as pd
+import io
 
-data = requests.get('https://observatory.middlebury.edu/campus/energy/')
+curr_time = datetime.now()
 
-print(data.content)
+curr_min = curr_time.minute
+curr_year = curr_time.year
+curr_month = curr_time.month
+#curr_day = curr_time.day
+
+url = "https://observatory.middlebury.edu/campus/energy/archive/"
+
+curr_day = 24
+curr_min = 1
+
+if (curr_min % 60 == 1):
+  url +=  "{}{}{}-campus.csv".format(curr_year, curr_month, curr_day)
+  print(url)
+  data = requests.get(url).content
+  print("Got Data")
+  df = pd.read_csv(io.StringIO(data.decode('utf-8')))
+  print(df)
