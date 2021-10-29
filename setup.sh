891b94888dead374f5666f1ffc79c8f4124fb038
walkerfrankenberg <wfrankenberg@middlebury.edu>
2021-10-29 09:23:23 -0400

    config file edits for thematic changes

--- setup.sh
+++ setup.sh
@@ -5,4 +5,11 @@ headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
+[theme]\n\
+primaryColor=\"#1D84B5\"\n\
+backgroundColor=\"#EBEBEB\"\n\
+secondaryBackgroundColor=\"#0A2239\"\n\
+textColor=\"#0F3457\"\n\
+font=\"sans serif\"\n\
+\n\
 " > ~/.streamlit/config.toml
